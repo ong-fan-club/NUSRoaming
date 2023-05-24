4aeff1a894f5b367113a5d5509a1c8bb0300de3b
lordidiot <0xlordidiot@gmail.com>
2023-05-23 21:39:24 -0700

    Index page

---
+++ frontend/src/components/Logo.tsx
@@ -0,0 +1,7 @@
+export default function Logo () {
+  return ( 
+    <div className='text-3xl font-extrabold mx-12'>
+      <span className='text-main-blue'>NUS</span><span className='text-dark-grey'>Roaming</span>
+    </div>
+  );
+}
---
+++ frontend/src/components/NavBar.tsx
@@ -0,0 +1,35 @@
+import Link from 'next/link';
+import Logo from './Logo'
+
+interface ItemProps {
+  to: string;
+}
+
+function NavItem(props: ItemProps) {
+  return (
+    <Link href={'/' + props.to}>
+      <div className='capitalize text-xl text-dark-grey mx-4 my-3'>
+        {props.to}
+      </div>
+    </Link>
+  )
+
+}
+
+function NavLinks() {
+  const navItems = [ 'countries', 'schools', 'about' ]
+  return (
+    <div className='flex flex-row mr-12'>
+      { navItems.map( x => <NavItem to={x} /> ) }
+    </div>
+  )
+}
+
+export default function NavBar () {
+  return ( 
+    <div className='w-full h-32 flex flex-row items-center justify-between'>
+      <Logo />
+      <NavLinks />
+    </div>
+  );
+}
