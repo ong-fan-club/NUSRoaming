c25a17fed299aa87362ba825173805c313dd7d16
lordidiot <0xlordidiot@gmail.com>
2023-06-20 22:59:47 -0700

    add basic page layouts

---
+++ frontend/src/app/countries/page.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+
+function CountryCard(country: string) {
+  const href = `/countries/${country}`
+  return (
+    <Link href={href}>
+      <div className="h-24 px-12 shadow rounded-lg flex flex-col justify-center items-center bg-gray-700">
+        <div className="text-white font-semibold text-lg">{country}</div>
+      </div>
+    </Link>
+  );
+}
+
+export default async function Page() {
+  const url = `http://localhost:8000/query?query=SELECT DISTINCT university_country FROM partner_unis`;
+  const countries: string[] = (await (await fetch(url)).json()).map((x: {university_country: string}) => x.university_country);
+  console.log(countries)
+
+  return (
+    <main className="flex flex-col items-center pt-12 w-full max-w-5xl mx-auto">
+      <div className="mt-12 flex flex-row flex-wrap w-full gap-x-4 gap-y-12 justify-start">
+        {countries.map((country) => CountryCard(country))}
+      </div>
+    </main>
+  );
+}
