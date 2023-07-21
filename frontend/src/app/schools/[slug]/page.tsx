9e1db1133fd6e2fc561c47713600f15b7781ee18
lordidiot <0xlordidiot@gmail.com>
2023-07-20 22:26:50 -0700

    school calendar + restyle

--- frontend/src/app/schools/[slug]/page.tsx
+++ frontend/src/app/schools/[slug]/page.tsx
@@ -15,8 +15,8 @@ interface SchoolInfo {
   faculties_accepted: string[];
   module_restrictions: string;
   visa_info: string;
-  sem_1_months: string[];
-  sem_2_months: string[];
+  sem1_months: string[];
+  sem2_months: string[];
   gpt_university_description: string;
   gpt_university_address: string;
   gpt_university_city: string;
@@ -44,59 +44,85 @@ export default async function Page({ params }: PageProps) {
             </Link>
           </b>
         </h2>
-        <UniversityAvailability availability={[true, true, false, false, false, false, false, false, false, false, false, false]} />
         <div>
-          {school.gpt_university_description}
-          <br />
-          <br />
-          {school.faculties_accepted.length > 0 && (
-            <h3>Suitable for Faculties:</h3>
-          )}
+          <div className="flex flex-row w-full mt-4 border rounded-lg px-4 py-3 pb-6">
+            <div className="w-1/2 pr-4">
+              <h3 className="text-xl font-semibold">Description</h3>
+              {school.gpt_university_description}
+            </div>
+            <div className="w-1/2">
+              <h3 className="text-xl font-semibold">School Calendar</h3>
+              <UniversityAvailability sem1_months={school.sem1_months} sem2_months={school.sem2_months} />
+            </div>
 
-          <ul className="list-disc list-inside">
-            {school.faculties_accepted.map((f) => (
-              <li>{f}</li>
-            ))}{" "}
-          </ul>
-          <div className="mt-6">
-            <h3>Find out more:</h3>
-            <ul className="list-disc list-inside">
-              {school.university_website.map((l) => (
-                <li>
-                  <Link className="text-blue-700" href={l}>
-                    {l}
-                  </Link>
-                </li>
-              ))}
-            </ul>
           </div>
+
+          <div className="w-full mt-4 border rounded-lg px-4 py-3">
+            <div>
+              {school.faculties_accepted.length > 0 && (
+                <div>
+                  <h3>Suitable for Faculties:</h3>
+                </div>
+              )}
+              <ul className="list-disc list-inside mb-4">
+                {school.faculties_accepted.map((f) => (
+                  <li>{f}</li>
+                ))}{" "}
+              </ul>
+            </div>
+            <div className="">
+              <h3>Find out more:</h3>
+              <ul className="list-disc list-inside">
+                {school.university_website.map((l) => (
+                  <li>
+                    <Link className="text-blue-700" href={l}>
+                      {l}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+
         </div>
 
         <div className="mt-6">
           <h2 className="text-2xl">
             Fast facts about <b>{school.gpt_university_city}</b>:
           </h2>
-          <br />
-          <ul>
-            <li>{school.gpt_location_description}</li>
-            <br />
-            <li>
-              <b>Safety: </b>
-              {school.gpt_location_crime}
-            </li>
-            <li>
-              <b>Cost of Living: </b>
-              {school.gpt_location_cost_of_living}
-            </li>
-            <li>
-              <b>Weather: </b>
-              {school.gpt_location_weather}
-            </li>
-            <li>
-              <b>Transportation: </b>
-              {school.gpt_location_transportation}
-            </li>
-          </ul>
+          <div className="flex flex-col w-full mt-4 border rounded-lg px-4 py-3">
+            <div className="w-full">
+              {school.gpt_location_description}
+            </div>
+            <div className="flex flex-row mt-4">
+              <div className="w-1/2 pr-4">
+                <h4 className="text-lg font-semibold">Safety</h4>
+                <p>
+                  {school.gpt_location_crime}
+                </p>
+              </div>
+              <div className="w-1/2">
+                <h4 className="text-lg font-semibold">Cost of living</h4>
+                <p>
+                  {school.gpt_location_cost_of_living}
+                </p>
+              </div>
+            </div>
+            <div className="flex flex-row mt-4">
+              <div className="w-1/2 pr-4">
+                <h4 className="text-lg font-semibold">Weather</h4>
+                <p>
+                  {school.gpt_location_weather}
+                </p>
+              </div>
+              <div className="w-1/2">
+                <h4 className="text-lg font-semibold">Transportation</h4>
+                <p>
+                  {school.gpt_location_transportation}
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
       <div id="comments" className="w-full">
--- frontend/src/components/UniversityAvailability.tsx
+++ frontend/src/components/UniversityAvailability.tsx
@@ -1,26 +1,56 @@
 import React from 'react';
 
 interface UniversityAvailabilityProps {
-  availability: boolean[]; // An array representing availability for each month (e.g., [Jan, Feb, Mar, ..., Dec])
+  sem1_months: string[];
+  sem2_months: string[];
 }
 
-const UniversityAvailability: React.FC<UniversityAvailabilityProps> = ({ availability }) => {
+const UniversityAvailability: React.FC<UniversityAvailabilityProps> = ({ sem1_months, sem2_months }) => {
   const monthLabels = [
     'January', 'February', 'March', 'April',
     'May', 'June', 'July', 'August',
     'September', 'October', 'November', 'December',
   ];
 
+  const monthLabelsShort = [
+    'JAN', 'FEB', 'MAR', 'APR',
+    'MAY', 'JUN', 'JUL', 'AUG',
+    'SEP', 'OCT', 'NOV', 'DEC',
+  ];
+
+  // const availableMonthIndices = [...sem1_months.map(x => monthLabelsShort.indexOf(x)), ...sem2_months.map(x => monthLabelsShort.indexOf(x))]
+  const sem1_indices: number[] = sem1_months.map(x => monthLabelsShort.indexOf(x))
+  const sem2_indices: number[] = sem2_months.map(x => monthLabelsShort.indexOf(x))
+
+  const availabilityColour = function(index: number) {
+    console.log(index, index in sem1_indices, sem1_indices)
+    if (sem1_indices.includes(index)) {
+      return 'bg-green-500';
+    } else if (sem2_indices.includes(index)) {
+      return 'bg-blue-500';
+    } else {
+      return 'bg-gray-500';
+    }
+  }
+
+
   return (
-    <div className="university-availability flex flex-wrap">
-      {monthLabels.map((month, index) => (
-        <div
-          key={month}
-          className={`month p-2 m-1 rounded font-bold ${availability[index] ? 'bg-green-500 text-white' : 'bg-gray-500 text-white'}`}
-        >
-          {month}
-        </div>
-      ))}
+    <div className='university-availability'>
+      <div className="flex flex-wrap">
+        {monthLabels.map((month, index) => (
+          <div
+            key={month}
+            className={`month p-2 m-1 rounded font-bold ${availabilityColour(index)} text-white`}
+          >
+            {month}
+          </div>
+        ))}
+      </div>
+      <div className='mt-4'>
+        Legend:
+        <span className='p-2 font-bold rounded text-white bg-green-500 ml-3'>SEM1</span>
+        <span className='p-2 font-bold rounded text-white bg-blue-500 ml-3'>SEM2</span>
+      </div>
     </div>
   );
 };
